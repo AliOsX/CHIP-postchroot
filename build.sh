905f311330f2489ffa2ca5200ee53c24ecb68ad9
Wynter Woods <zerotri@gmail.com>
2016-04-28 13:07:41 -0700

    Fix bug in sed command to change plymouth theme

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ mv -f /etc/rc.local.orig /etc/rc.local\n" |sudo tee rootfs/etc/rc.local >/dev/nu
 	sudo sed -i -e 's/#SystemKeepFree=.*/SystemKeepFree=5M/' rootfs/etc/systemd/journald.conf
 	sudo sed -i -e 's/#RuntimeMaxUse=.*/RuntimeMaxUse=10M/' rootfs/etc/systemd/journald.conf
 	sudo sed -i -e 's/#RuntimeKeepFree=.*/RuntimeKeepFree=5M/' rootfs/etc/systemd/journald.conf
-	sudo sed -i -e 's/#Theme==.*/Theme==tribar/' rootfs/etc/plymouth/plymouthd.conf
+	sudo sed -i -e 's/#Theme=.*/Theme=tribar/' rootfs/etc/plymouth/plymouthd.conf
 	sudo sed -i '/_initrd="$2"/a echo "Generating U-Boot image from $2"\nmkimage -A arm -T ramdisk -C none -d "$2" \/boot\/initrd.uimage' rootfs/etc/initramfs/post-update.d/flash-kernel
 
 	#network-manager should ignore wlan1
